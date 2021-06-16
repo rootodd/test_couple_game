d7c00fbc3355796c14cc9f23286245b4d8c95711
key <elesds.mooz9527@gmail.com>
2021-06-17 00:31:42 +0900

    基本的なホーム画面を実装

--- lib/main.dart
+++ lib/main.dart
@@ -13,53 +13,176 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AppPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+class AppPage extends StatelessWidget{
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Couple Game',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        scaffoldBackgroundColor: Color(0xFFb70086),
+      ),
+      home: CoupleGameHome(),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CoupleGameHome extends StatelessWidget{
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  String player_1 = '';
+  String player_2 = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // キーボードを表示した時にレイアウトを崩さない
+      resizeToAvoidBottomInset:  false,
+
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('カップルゲーム'),
+        backgroundColor: Color(0xFFb70086),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+
+      body: Container(
+        // 背景のグラデーション設定
+        // 背景のグラデーションを設定
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: FractionalOffset.topLeft,
+            end: FractionalOffset.bottomRight,
+            colors: [
+              const Color(0xffe4a972).withOpacity(0.6),
+              const Color(0xff9941d8).withOpacity(0.6),
+            ],
+            stops: const [
+              0.0,
+              1.0,
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                  padding: EdgeInsets.all(4.0),
+                child: Text(
+                    '',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              // プレイヤー名1の入力フィールド
+              Padding(
+                padding: EdgeInsets.only(top: 10, right: 40, bottom: 0, left: 40),
+                child: TextFormField(
+                  onChanged:(name_1){
+                    this.player_1 = name_1;
+                  },
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10.0,
+                  ),
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.legend_toggle,
+                      color: Colors.white,
+                    ),
+                    border: OutlineInputBorder(),
+                    labelText: '女性の名前',
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                    // 外枠の装飾
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 0, right: 40, bottom: 0, left: 40),
+                child: Text(
+                  '',
+                  style: TextStyle(
+                      color: Colors.white
+                  ),),
+              ),
+              // プレイヤー名2の入力フィールド
+              Padding(
+                padding: EdgeInsets.only(top: 0, right: 40, bottom: 0, left: 40),
+                child: TextFormField(
+                  onChanged:(name_2){
+                    this.player_2 = name_2;
+                  },
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10.0,
+                  ),
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.access_alarm,
+                      color: Colors.white,
+                    ),
+                    border: OutlineInputBorder(),
+
+                    labelText: '男性の名前',
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                    // 外枠の装飾
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical:   20.0,
+                ),
+                child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+//                        builder: (context) => GamePage(player_1:player_1, player_2:player_2),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'ゲームスタート',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFF930020),
+                    onPrimary: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
